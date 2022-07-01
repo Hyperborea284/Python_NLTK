425d6fb1b7782d0bbe80f140bb98b42b3f0b94bb
Hyperborea284 <000jhs@gmail.com>
2022-07-01 11:59:24 -0300

    Update freqs.py
    Funções semânticas

--- freqs.py
+++ freqs.py
@@ -4,6 +4,8 @@ from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 import string
 import matplotlib.pyplot as plt
+import pyfiglet
+import os
 
 class Loader:
     def __init__(self):
@@ -37,6 +39,33 @@ class Loader:
         plt.title("Frequencia das 50 palavras mais recorrentes")
         plt.show()
 
+    @staticmethod
+    def semantic_stuff(words):
+
+        os.system('clear')
+        print(f'{pyfiglet.figlet_format("Text_Proc")}\n\n\n\n            Insira o termo-chave que será buscado : \n\n\n\n')
+        user_input_term = input("Option  : ")
+
+        os.system('clear')
+        print(f'{pyfiglet.figlet_format("Text_Proc")}\n\n\n\n            1) Opção     1 - Similaridades\
+                                                     \n\n\n\n            2) Opção     2 - Concordâncias\
+                                                     \n\n\n\n            3) Opção     3 - Bigramas\
+                                                     \n\n\n\n            4) Opção     4 - Trigramas\
+                                                     \n\n\n\n')
+        user_input = input("Opção  : ")
+
+        if user_input == '1':
+            print(words.similar(user_input_term))
+        elif user_input == '2':
+            print(words.concordance(user_input_term))
+        elif user_input == '3':
+            print(list(nltk.bigrams(words)))
+        elif user_input == '4':
+            print(list(nltk.trigrams(words)))
+        else:
+            os.system('clear')
+
 corpus = Loader.words_loader()
 clean = Loader.stopwords_cleaner(corpus)
-freq = Loader.freq_stuff_dist_50(clean)
+#freq = Loader.freq_stuff_dist_50(clean)
+semantic = Loader.semantic_stuff(corpus)
